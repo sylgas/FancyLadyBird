531992fb9533022a57dc331e18033940957a8ec4
Sylgas <sylwia.m.gasior@gmail.com>
2013-11-09 20:25:24 +0100

    First commit

---
+++ gen/com/screens/FancyLadyBird/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.screens.FancyLadyBird;
+
+/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
+public final class R {
+}
